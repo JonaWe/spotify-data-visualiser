b2a4dd71d8fc1551b2a85198325789287010dc8e
Jona Wessendorf <jona.wessendorf1@uni-bielefeld.de>
2021-06-26 23:33:32 +0200

    adds top ten chart for tracks

--- components/dataViews/Top10BarChart.js
+++ components/dataViews/Top10BarChart.js
@@ -10,7 +10,7 @@ import {
   ResponsiveContainer,
 } from 'recharts';
 
-export default function TopArtistsChart({ data }) {
+export default function Top10BarChart({ data, dataKey }) {
   return (
     <div style={{ width: '80vw', height: '80vh' }}>
       <ResponsiveContainer width="100%" height="100%">
@@ -26,7 +26,7 @@ export default function TopArtistsChart({ data }) {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="artistName" />
+          <XAxis dataKey={dataKey} />
           <YAxis />
           <Tooltip />
           <Legend />
--- components/dataViews/index.js
+++ components/dataViews/index.js
@@ -1,12 +1,11 @@
 import DataProcessor from '../../lib/DataProcessor';
 import TimeslotsRadarChart from './TimeslotsRadarChart';
 import WeekdaysRadarChart from './WeekdaysRadarChart';
-import TopArtistsChart from './TopArtistsChart';
+import Top10BarChart from './Top10BarChart';
 import NumberWithUnit from '../NumberWithUnit';
 
 export default function dataViews({ streamingHistory }) {
   const dataProcessor = new DataProcessor(streamingHistory);
-  const topArtist = dataProcessor.getTopArtist();
   return (
     <>
       <h1>Stats for the past year</h1>
@@ -29,12 +28,22 @@ export default function dataViews({ streamingHistory }) {
         value={dataProcessor.getTotalSkippedSongs()}
       />
       <NumberWithUnit
-        prefix={`Top Artist is '${topArtist.artistName}' with `}
-        value={topArtist.msPlayed}
+        prefix={`Top Artist is '${
+          dataProcessor.getTopArtist().artistName
+        }' with a playtime of `}
+        value={dataProcessor.getTopArtist().msPlayed}
         unit="days"
       />
-      <TopArtistsChart data={dataProcessor.getTopArtists()} />
+      <h2>Top 10 Artists by Playtime</h2>
+      <Top10BarChart
+        data={dataProcessor.getTopArtists()}
+        dataKey="artistName"
+      />
+      <h2>Top 10 Tracks by Playtime</h2>
+      <Top10BarChart data={dataProcessor.getTopTracks()} dataKey="trackName" />
+      <h2>Listening Activity related to Daytime</h2>
       <TimeslotsRadarChart data={dataProcessor.getTimeslots()} />
+      <h2>Listening Activity related to Weekday</h2>
       <WeekdaysRadarChart data={dataProcessor.getWeekdays()} />
     </>
   );
