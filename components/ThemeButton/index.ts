7ab732fd0caf9ec5cdfc31e92bba2e326050751f
Jona Wessendorf <jonawessendorf@gmail.com>
2021-12-09 15:06:25 +0100

    converts ThemeButton to ts and makes its size fixed when zooming

--- components/ThemeButton/index.ts
+++ components/ThemeButton/index.ts
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 export const ThemeButton = styled.span`
   display: grid;
   padding: 0.35em;
-  font-size: 2em;
+  font-size: 2vw;
   color: ${(props) => props.theme.bgPrimary};
   background-color: ${(props) => props.theme.fcLight};
   border-radius: 100vh;
-  box-shadow: 5px 5px 15px
-    ${(props) => (props.theme.isDarkTheme ? '3px' : '-1px')} rgba(0, 0, 0, 0.5);
+  box-shadow: 0.25vw 0.25vw 1.5vw
+    ${(props) => (props.theme.isDarkTheme ? '0.3vw' : '-0.1vw')}
+    rgba(0, 0, 0, 0.5);
 
   position: fixed;
   bottom: 0;
