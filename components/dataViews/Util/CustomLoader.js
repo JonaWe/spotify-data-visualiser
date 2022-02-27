f14e06173361e80fec3cbde4ea7162a3fde47b00
Jona Wessendorf <jonawessendorf@gmail.com>
2022-02-27 16:06:31 +0100

    updates package versions

--- components/dataViews/Util/CustomLoader.js
+++ components/dataViews/Util/CustomLoader.js
@@ -1,15 +1,8 @@
 import { ThemeContext } from 'styled-components';
 import { useContext } from 'react';
-import Loader from 'react-loader-spinner';
+import { ThreeDots } from 'react-loader-spinner';
 
 export default function CustomLoader() {
   const theme = useContext(ThemeContext);
-  return (
-    <Loader
-      type="ThreeDots"
-      color={theme.accentColor}
-      height={100}
-      width={100}
-    />
-  );
+  return <ThreeDots color={theme.accentColor} height={100} width={100} />;
 }
